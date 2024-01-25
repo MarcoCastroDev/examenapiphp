c59f3c425016c9bd3dc2797b3bdaa8546aacbff1
MarcoCastroDev <ingcastrodev@gmail.com>
2024-01-25 08:22:52 -0700

    cambios en el head
    se cambio icono y el titulo de la página principal

--- test.php
+++ test.php
@@ -9,8 +9,9 @@ require('opcionesFiltrado.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inventario - Marco</title>
-    <link rel="shortcut icon" href="img\Senor-frogs-logo-removebg-preview.png" />
+    <title>Reporte de
+        OneBeat vs Buffer</title>
+    <link rel="shortcut icon" href="img\faviconTGC.ico" />
     <!-- Llamado de Bootsrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
